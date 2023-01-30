0b77bbf1bcbd500361cbf2fda47ada9c449795d3
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2023-01-30 13:50:04 +0200

    Full final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,14 +15,16 @@ export const App = () => {
   const onClick = option => {
     switch (option) {
       case 'good':
-        setGood(good + 1);
+        setGood(prev => prev + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
         break;
       default:
-        setBad(bad + 1);
-        break;
+        return;
     }
   };
 
